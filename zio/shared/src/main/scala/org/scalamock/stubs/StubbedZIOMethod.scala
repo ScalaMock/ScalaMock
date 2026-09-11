3e4155d247b46f18fc5e263100d509d185916113
Georgii Kovalev <goshacodes@gmail.com>
2026-09-11 21:51:59 +0500

    Add returnsWhen stubbing method
    Adds a returnsWhen(pf: PartialFunction[A, R]) method to StubbedMethod
    (and its Cats Effect / ZIO counterparts) allowing a stub to be defined
    for a subset of argument values only. Calling the stub with an
    argument for which the partial function is not defined throws a new
    StubNotImplementedError describing the failing method and argument.

--- zio/shared/src/main/scala/org/scalamock/stubs/StubbedZIOMethod.scala
+++ zio/shared/src/main/scala/org/scalamock/stubs/StubbedZIOMethod.scala
@@ -221,6 +221,8 @@ class StubbedZIOMethod[A, R](delegate: StubbedMethod[A, R]) extends StubbedMetho
    * */
   def returnsWith(value: => R) = delegate.returnsWith(value)
 
+  def returnsWhen(pf: PartialFunction[A, R]): Unit = delegate.returnsWhen(pf)
+
   /** Allows to set result depending on call number starting from 1
    *
    * Scala 3
